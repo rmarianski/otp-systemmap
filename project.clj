31961a92c21fbd90f8c2adeb41c14e5504176e9e
Robert Marianski <rob@marianski.com>
2010-04-23 09:24:20 -0400

    updated project dependencies

--- project.clj
+++ project.clj
@@ -6,11 +6,12 @@
                  [org.slf4j/slf4j-api "1.5.2"]
                  [compojure "0.3.2"]
                  [swank-clojure "1.1.0"]
+                 [commons-beanutils "1.8.3"]
                  ; otp stuff isn't necessary
                  ;[org.opentripplanner/opentripplanner-routing "0.3-SNAPSHOT"]
                  ;; the extended one gives me problems
                  ;; and it's because it's a war file and not a jar
                  ;[org.opentripplanner/opentripplanner-api-extended "0.3-SNAPSHOT"]
-                 [org.onebusaway/onebusaway-gtfs "1.1.0"]]
+                 [org.onebusaway/onebusaway-gtfs "1.1.2"]]
   :dev-dependencies [[uk.org.alienscience/leiningen-war "0.0.2"]]
   :namespaces [otp.core])
