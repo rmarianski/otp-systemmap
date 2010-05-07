fccc9f46b5068c94efd3c2a9e7fb18688c2a42fd
Robert Marianski <rob@marianski.com>
2010-05-06 23:32:47 -0400

    updated gtfs path

--- src/otp/config.clj
+++ src/otp/config.clj
@@ -1,4 +1,6 @@
-{:gtfs-file-path "/home/rob/data/otp/mta/theonetrain.zip"
+; "/home/rob/data/otp/mta/nyct_subway_100308.zip"
+; "/home/rob/data/otp/mta/theonetrain.zip"
+{:gtfs-file-path "/home/rob/data/otp/mta/nyct_subway_100308.zip"
  :default-agency-id "MTA NYCT"
  :geoserver-base-uri "http://openlayers.example.com/geoserver/wms"
  :url-prefix "/opentripplanner-api-extended"
